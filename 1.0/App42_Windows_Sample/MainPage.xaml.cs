4060b24d642b3fb18350036b85af5d4bc1f34e56
himanshu sharma <himanshu.sharma@shephertz.co.in>
2013-10-29 19:39:38 +0530

    1.0 Client artifacts
    Signed-off-by: himanshu sharma <himanshu.sharma@shephertz.co.in>

---
+++ 1.0/App42_Windows_Sample/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+// To use App42 Api's yyou have to import some packages
+
+using com.shephertz.app42.paas.sdk.windows;
+using com.shephertz.app42.paas.sdk.windows.user;
+// This is the main page of your app
+
+namespace Sample
+{
+    public partial class MainPage : PhoneApplicationPage, App42Callback
+    {
+        static ServiceAPI serviceApi = new ServiceAPI("YOUR_API_KEY", "YOUR_SECRET_KEY");
+        // Here you have to use your apiKey and secretKey in order to obtain connection from App42 Server
+        
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // this is the click event
+
+        private void login_Click_1(object sender, RoutedEventArgs e)
+        {
+            String userName = this.username.Text;
+            String password = this.password.Password;
+            String email = this.email.Text;
+            serviceApi.SetBaseURL("YOUR API SERVER URL");
+            // here you have to build the service u want to use
+
+            UserService userService = serviceApi.BuildUserService();
+            userService.CreateUser(userName, password, email, this);
+            // now using this userService you can use all functions of User Service
+            // this represents App42Callbaack object
+            // now you have to explictlly implements methods of App42Callback
+            
+        }
+
+        void App42Callback.OnException(App42Exception exception)
+        {
+            // here Exception is handled
+            Dispatcher.BeginInvoke(() => { textB.Text = "Exception Caught successfully"; });
+        }
+
+        void App42Callback.OnSuccess(object response)
+        {
+            // here success is shown
+            Dispatcher.BeginInvoke(() => { textB.Text = "User Successfully Created"; });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,45 @@
 App42_WINDOWS_SDK
-=================
+================
 
-App42 Cloud API Client SDK files for Windows Phone
+App42 BPaaS Cloud API Client SDK files for WINDOWS
 
-[Download the latest App42 Windows Phone SDK] (https://github.com/shephertz/App42_WINDOWS_SDK/raw/master/1.7/app42_windows_1.7.zip)
+- Download the App42 BPaaS WINDOWS SDK
+- Unzip the file and open **App42 WINDOWS Sample** project.
+- Add the **App42_BPaaS_WINDOWS_SDK_x.x.x.dll** into refrence. (**Add Reference -> Select Your dll path)**
+- Initialize the library using
+```
+ServiceAPI sp = new ServiceAPI("<YOUR_API_KEY>","<YOUR_SECRET_KEY>");
+sp.SetBaseURL("<YOUR_API_SERVER_URL>");
+```
+- Instantiate the service that one wants to use in the App, e.g. using User service one has to do the following
+```
+UserService userService = sp.BuildUserService();
+```
 
-[Documentation and API guide] (http://api.shephertz.com/app42-dev/windows-phone-backend-apis.php)
+- Now one can call associated method of that service e.g. user creation can be done with the following snippet
+
+```
+public class Callback : App42Callback  
+{  
+	public CallBack(){
+		String userName = "Nick";   
+		String pwd  = "*****";
+		String emailId = "nick@shephertz.co.in";
+		userService.CreateUser(userName,pwd,emailId, this);  
+	}
+	void App42Callback.OnException(App42Exception exception)  
+	{  
+		Console.WriteLine("Exception Message");  
+	}  
+	void App42Callback.OnSuccess(Object response)  
+	{  
+		User user = (User) response;
+		Console.WriteLine("userName is " + user.GetUserName());
+		Console.WriteLine("emailId is " + user.GetEmail());     
+		String jsonResponse = user.ToString();  
+	}  
+}  
+
+```
+
+- Build the project and run.
