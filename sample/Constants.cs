3c504627bb50f2949e73213af6f08ed7a053c500
SamitaMShephertz <samita.mahajan@shephertz.co.in>
2014-01-23 17:21:41 +0530

    sample updated
    Signed-off-by: SamitaMShephertz <samita.mahajan@shephertz.co.in>

---
+++ sample/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App42Sample
+{
+    class Constants
+    {
+        public static String apiKey = "API_KEY";
+        public static String secretKey = "SECRET_KEY";
+        public static String gameName = "Devil May Cry";
+        public static String dbName = "queryTest";
+        public static String collectionName = "WindowsTestCollection";
+        public static String uname = null;
+        public static String userName = "Nick";
+        public static String fileName1, fileName2 = null;
+        public static String gameUserName = "Nick";
+    }
+}
---
+++ sample/ScoreListItem.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace App42Sample
+{
+    public class ScoreListItem
+    {
+        private string _icon = "/Images/user.png";
+        public string Icon
+        {
+            get
+            {
+                return _icon;
+            }
+            set
+            {
+                if (value != _icon)
+                {
+                    _icon = value;
+                    NotifyPropertyChanged("Icon");
+                }
+            }
+        }
+
+        private string _userid;
+        public string UserId
+        {
+            get
+            {
+                return _userid;
+            }
+            set
+            {
+                if (value != _userid)
+                {
+                    _userid = value;
+                    NotifyPropertyChanged("UserId");
+                }
+            }
+        }
+
+        private string _rank;
+        public string Rank
+        {
+            get
+            {
+                return _rank;
+            }
+            set
+            {
+                if (value != _rank)
+                {
+                    _rank = value;
+                    NotifyPropertyChanged("Rank");
+                }
+            }
+        }
+
+        private string _username;
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (value != _username)
+                {
+                    _username = value;
+                    NotifyPropertyChanged("UserName");
+                }
+            }
+        }
+
+        private string _score;
+        public string Score
+        {
+            get
+            {
+                return _score;
+            }
+            set
+            {
+                if (value != _score)
+                {
+                    _score = value;
+                    NotifyPropertyChanged("Score");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
